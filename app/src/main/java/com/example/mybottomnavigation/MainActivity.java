d94cb8f7882143cb7060e711f463b80e65f8f2e5
Dery Sudrajat <dery.sudrajat17@gmail.com>
2019-05-11 14:25:39 +0700

    Add Bottom Navigation Bar Project

---
+++ app/src/main/java/com/example/mybottomnavigation/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.mybottomnavigation;
+
+import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment mFragment;
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mFragment = new HomeFragment();
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.container_layout,mFragment, mFragment.getClass().getSimpleName()).commit();
+                    return true;
+                case R.id.navigation_dashboard:
+                    mFragment = new DashboardFragmen();
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.container_layout, mFragment, mFragment.getClass().getSimpleName()).commit();
+                    return true;
+                case R.id.navigation_notifications:
+                    mFragment = new NotificationsFragment();
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.container_layout, mFragment, mFragment.getClass().getSimpleName()).commit();
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        if (savedInstanceState == null){
+            navView.setSelectedItemId(R.id.navigation_home);
+        }
+    }
+
+}
